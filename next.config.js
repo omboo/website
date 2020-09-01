c615907be540e349bad34335ca8bd7e80b2992bc
Sebastian Rodriguez <sebasrodriguez@gmail.com>
2020-09-01 15:31:29 -0300

    wip: Testing bundle size check

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
+const PacktrackerPlugin = require('@packtracker/webpack-plugin');
 const withImages = require('next-images');
 
 module.exports = {
   ...withImages(),
   target: 'serverless',
+  webpack: (config, { isServer }) => {
+    if (!isServer && process.env.GITHUB_EVENT_PATH) {
+      config.plugins.push(new PacktrackerPlugin({ upload: true }));
+    }
+    return config;
+  },
 };
