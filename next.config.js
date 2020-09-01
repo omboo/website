94dda749b5a2014a3e976cdfbc3d6aba4a531e64
Sebastian Rodriguez <sebasrodriguez@gmail.com>
2020-09-01 15:37:27 -0300

    fix: Updated packtracker configuration

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+/* eslint-disable import/no-dynamic-require */
+/* eslint-disable global-require */
+/* eslint-disable radix */
 const PacktrackerPlugin = require('@packtracker/webpack-plugin');
 const withImages = require('next-images');
 
@@ -6,7 +9,19 @@ module.exports = {
   target: 'serverless',
   webpack: (config, { isServer }) => {
     if (!isServer && process.env.GITHUB_EVENT_PATH) {
-      config.plugins.push(new PacktrackerPlugin({ upload: true }));
+      const event = require(process.env.GITHUB_EVENT_PATH);
+      config.plugins.push(
+        new PacktrackerPlugin({
+          upload: true,
+          fail_build: true,
+          branch: event.ref.replace('refs/heads/', ''),
+          author: event.head_commit.author.email,
+          message: event.head_commit.message,
+          commit: process.env.GITHUB_SHA,
+          committed_at: parseInt(+new Date(event.head_commit.timestamp) / 1000),
+          prior_commit: event.before,
+        }),
+      );
     }
     return config;
   },
